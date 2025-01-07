4059f5ce82b34ff44a0b4b72dd4eca7bae475e45
Rishan <rishan.shrestha@aeritech.com>
2025-01-07 15:37:19 +0545

    few changes in bat file

--- run-test.bat
+++ run-test.bat
@@ -2,6 +2,10 @@
 
 start "" cmd.exe /k pytest %1 --html=report.html --dashboard --archive-logs --recorder -v -s --junit-xml=junit/test-results.xml --headless
 
+if not exist "dashboard.html" (
+timeout /t 3
+)
+
 start "" python -m http.server 1948
 
 start "" http://localhost:1948/dashboard.html
